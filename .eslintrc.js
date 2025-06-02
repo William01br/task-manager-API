b6f5fc7eb2421e9b77d8b2cd29a6c77ff4ccac87
William01br <williamstcorreia123@gmail.com>
2025-06-02 10:19:11 -0300

    chore: add husky config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs', 'jest.config.js', 'test/jest.config.js'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/only-throw-error': 'error',
+    },
+  },
+  eslintConfigPrettier,
+);
