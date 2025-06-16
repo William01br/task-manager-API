22776c8cb457ae08bc4b375aec09ce5b614c26cf
William01br <williamstcorreia123@gmail.com>
2025-06-16 14:23:50 -0300

    feat(schema): add zod schemas for limit and page for endpoint 'GET /api/tasks/:page/:limit'

---
+++ src/application/schemas/LimitSchema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const max = 100;
+
+export const limitSchema = z
+  .string()
+  .regex(/^[0-9]*$/, { message: 'The limit must be a valid number.' })
+  .transform(Number)
+  .refine((val) => isValidValue(val), {
+    message: `The limit should be greater than 0 and not exceed ${max}`,
+  });
+
+function isValidValue(val: number): boolean {
+  if (val < 1) return false;
+  if (val > 100) return false;
+  return true;
+}
