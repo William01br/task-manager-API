2f587ba6eec58c5a0aa43c04720adb42c06b9950
William01br <williamstcorreia123@gmail.com>
2025-06-19 15:46:49 -0300

    fix(Task): modifies zod schema for validate date in format ISO

--- src/application/schemas/TaskResponseDTO.ts
+++ src/application/schemas/TaskResponseDTO.ts
@@ -1,17 +1,11 @@
 import { z } from 'zod';
 import { TaskCreateSchema } from './TaskCreateSchema';
-import { Types } from 'mongoose';
 
 export const TaskResponseSchema = TaskCreateSchema.merge(
   z.object({
-    id: z.preprocess((val) => {
-      if (val instanceof Types.ObjectId) return val.toString();
-      return val;
-    }, z.string()),
+    id: z.string(),
     isDone: z.boolean(),
-    createdAt: z.date(),
-    updatedAt: z.date(),
+    createdAt: z.string().datetime(),
+    updatedAt: z.string().datetime(),
   }),
 );
-
-export type TaskResponseDTO = z.infer<typeof TaskResponseSchema>;
