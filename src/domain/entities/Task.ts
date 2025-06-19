6d4942107fa5364e8ba5e14e7cd43d494927c8f2
William01br <williamstcorreia123@gmail.com>
2025-06-19 15:42:34 -0300

    fix(Task): modifies the Task, TaskPreview, and TaskResponseDTO interfaces

--- src/domain/entities/Task.ts
+++ src/domain/entities/Task.ts
@@ -1,12 +1,34 @@
 // Representa a entidade "Task" de forma pura, com seus atributos e eventuais métodos que implementem regras de negócio.
 
-export class Task {
-  public title: string;
-  public description: string;
-  public isDone: boolean;
-  constructor(data: { title: string; description: string; isDone: boolean }) {
-    this.title = data.title;
-    this.description = data.description;
-    this.isDone = data.isDone;
-  }
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  isDone: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
+
+export interface TaskResponseDTO extends Omit<Task, 'createdAt' | 'updatedAt'> {
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type TaskPreview = Pick<Task, 'title' | 'description' | 'isDone'>;
+
+export type TaskCreateDTO = Pick<Task, 'title' | 'description'>;
+
+// export type TaskResponseDTO = Pick<
+//   Task,
+//   'id' | 'title' | 'description' | 'isDone'>;
+
+// export class Task {
+//   public title: string;
+//   public description: string;
+//   public isDone: boolean;
+//   constructor(data: { title: string; description: string; isDone: boolean }) {
+//     this.title = data.title;
+//     this.description = data.description;
+//     this.isDone = data.isDone;
+//   }
+// }
