12eeee545a0b570fa152a602f62434764942b13e
William01br <williamstcorreia123@gmail.com>
2025-06-21 14:01:01 -0300

    chore: add jest setup and config. Add alias for test directory in module-alias

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,14 @@ const root = resolve(__dirname);
 module.exports = {
   rootDir: root,
   displayName: 'root-tests',
+  setupFiles: ['<rootDir>/jest.setup.js'],
   testMatch: ['<rootDir>/test/**/*.spec.ts'],
-  testEnvorinment: 'node',
+  testEnvironment: 'node',
   clearMocks: true,
   preset: 'ts-jest',
   moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir:/src/$1',
-    '^@test/(.*)$': '<rootDir:/test/$1',
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
   },
   collectCoverageFrom: ['<rootDir>/test/**/*.ts'],
   coverageDirectory: 'coverage',
