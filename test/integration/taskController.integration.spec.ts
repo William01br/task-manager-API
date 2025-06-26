54d1eec341711f93c4a2b0a88af6c04860caf2d9
William01br <williamstcorreia123@gmail.com>
2025-06-26 09:08:03 -0300

    test: add integration test for TaskController in 'GET /api/tasks/:id' endpoint

--- test/integration/taskController.integration.spec.ts
+++ test/integration/taskController.integration.spec.ts
@@ -182,4 +182,74 @@ describe('TaskController integration tests', () => {
       });
     });
   });
+  describe('GET /api/tasks/:id', () => {
+    describe('when the id is valid and the task exist', () => {
+      it('should return status 200 and the task', async () => {
+        const request = await global.testRequest
+          .post('/api/tasks')
+          .send({
+            title: 'testing',
+            description: 'some text',
+          })
+          .expect(200);
+        const typedPostBody = request.body as { data: TaskResponseDTO };
+        const id = typedPostBody.data.id;
+
+        const { body } = await global.testRequest
+          .get(`/api/tasks/${id}`)
+          .expect(200);
+
+        const typedGetBody = body as { data: TaskResponseDTO };
+
+        expect(typedGetBody.data.id).toBe(id);
+        expect(typedGetBody.data.title).toBe('testing');
+      });
+    });
+    describe("when the param 'id' is invalid", () => {
+      describe('when the id not have 24 characters', () => {
+        it("should return status 400 and message error 'ID must have exactly 24 characters'", async () => {
+          const { body } = await global.testRequest
+            .get('/api/tasks/684984e34e832fe')
+            .expect(400);
+
+          expect(body.errors[0].context.issues[0].message).toBe(
+            'ID must have exactly 24 characters',
+          );
+        });
+      });
+      describe('when the id not is hexadecimal', () => {
+        it("should return status 400 and message error 'ID must be a valid hexadecimal'", async () => {
+          const { body } = await global.testRequest
+            .get('/api/tasks/684982b09g65ba21ada57ze6')
+            .expect(400);
+
+          expect(body.errors[0].context.issues[0].message).toBe(
+            'ID must be a valid hexadecimal',
+          );
+        });
+      });
+    });
+    describe('when the task not exist', () => {
+      it("should return status 404 and message 'Task not found'", async () => {
+        const { body } = await global.testRequest
+          .get('/api/tasks/684984e34e832fe4542c0e0a')
+          .expect(404);
+
+        expect(body.errors[0].message).toBe('Task not found');
+      });
+    });
+    describe('when occurs any internal error in server', () => {
+      it("should return status 500 and message 'Something went wrong'", async () => {
+        jest.spyOn(TaskModel, 'findById').mockImplementationOnce(() => {
+          throw new Error('fail simulation database');
+        });
+
+        const { body } = await global.testRequest
+          .get('/api/tasks/684984e34e832fe4542c0e0a')
+          .expect(500);
+
+        expect(body.errors[0].message).toBe('Something went wrong');
+      });
+    });
+  });
 });
