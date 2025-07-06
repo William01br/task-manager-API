a0c36cc4007c38a798f6772a12ed4a3dd65e7e6f
William01br <williamstcorreia123@gmail.com>
2025-07-06 08:07:21 -0300

    chore: create env test file and validate the environments variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,19 @@
-import 'dotenv/config';
+import path from 'node:path';
+
+import { config as loadEnv } from 'dotenv';
 import { z } from 'zod';
+
+// this will load and validate all variables
+loadEnv({ path: path.resolve(__dirname, '../../.env') });
+loadEnv({ path: path.resolve(__dirname, '../../.env.test'), override: true });
 
 const schema = z.object({
   ME_CONFIG_MONGODB_URL: z.string().nonempty().url(),
   ME_CONFIG_REDIS_URL: z.string().nonempty().url(),
+
+  // env tests
+  TEST_CONFIG_REDIS_URL: z.string().nonempty().url(),
+  TEST_CONFIG_MONGODB_URL: z.string().nonempty().url(),
 });
 
 const parsed = schema.safeParse(process.env);
