ccdf99eb799710a0066df2c32ab469665c8784d7
William01br <williamstcorreia123@gmail.com>
2025-07-13 13:28:43 -0300

    feat: add connection in startup application

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import { connectToRedis } from './config/cache';
 import { connectToMongoose } from './config/database';
 import { SetupServer } from './server';
+import logger from './logging/logger';
+import { connectToMongo } from './config/mongo';
 
 async function boostrap(): Promise<void> {
-  const server = new SetupServer();
   try {
+    const server = new SetupServer();
     server.init();
     server.start();
+
     await connectToMongoose();
+    logger.info('Connection to Mongoose established!');
+
     await connectToRedis();
+    logger.info('Connection to Redis established!');
+
+    await connectToMongo();
+    logger.info('Connection to MongoDB established!');
   } catch (error) {
-    console.error('Error starting the application:', error);
+    logger.error('Error starting the application:', error);
     process.exit(1);
   }
 }
